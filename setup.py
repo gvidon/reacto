611666416d94439114fbe173b678ce3245427173
gvidon <gvidon@ottofeller.com>
2016-10-19 17:55:48 +0300

    Added dependencies list

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 # !/usr/bin/env python
 from setuptools import find_packages, setup
 
-
 setup(
-	name='reacto',
-	packages=find_packages(),
-	version='0.1.0',
-	description='Set of commands to maintain react apps and components',
 	author='Artem Rudenko',
 	author_email='gvidon@ottofeller.com',
-	url='https://github.com/gvidon/reacto',
-	keywords=['react', 'js', 'tools', ],
+	description='Set of commands to maintain react apps and components',
 
 	entry_points={
   	'console_scripts': [
 			'reacto = reacto.main:cli',
 		],
 	},
+
+	install_requires=['click', 'cookiecutter',],
+	keywords=['react', 'js', 'tools', ],
+	name='reacto',
+	packages=['reacto',],
+	version='0.1.0',
+	url='https://github.com/gvidon/reacto',
 )
